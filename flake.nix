0a8a735b06e06f0527c5d3f88fbbae26dd122a42
neocrz <neo59crz@gmail.com>
2024-11-12 20:07:24 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "dotfiles";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      # pkgs = nixpkgs.legacyPackages.${system};
+      genPkgs = {n, s, o ? []}: import n {
+        system = s;
+        config.allowUnfree = true;
+        config.allowUnfreePredicate = pkg: true;
+        overlays = [  ] ++ o;
+      };
+    in {
+      homeConfigurations."eee" = 
+      let
+        pkgs = genPkgs { n = nixpkgs; s = system; };
+        pkgs-stable = genPkgs { n = nixpkgs-stable; s = system; };
+      in
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        extraSpecialArgs = {inherit inputs pkgs pkgs-stable system;};
+        modules = [ ./home/eee ];
+      };
+      
+      nixosConfigurations.a5 = 
+      let
+        pkgs = genPkgs { n = nixpkgs; s = system; };
+        pkgs-stable = genPkgs { n = nixpkgs-stable; s = system; };
+      in
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs pkgs pkgs-stable system;};
+        modules = [ ./hosts/a5 ];
+      };
+
+    };
+}
---
+++ hosts/a5/host-info.nix
@@ -0,0 +1,15 @@
+{
+  hostname = "a5";
+  system = "x86_64-linux";
+  model = "Acer aspire 5";
+  hybrid = "true";
+  busId = {
+    intel = "PCI:0:2:0";
+    nvidia = "PCI:2:0:0";
+  };
+  timeZone = "America/Sao_Paulo";
+  defaultLocale = "en_US.UTF-8";
+  extraLocale = "pt_BR.UTF-8";
+  keyMap = "br-abnt2";
+  layout = "br";
+}
