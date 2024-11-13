4872c46dec450a4b06c86c1625498c806552347c
neocrz <neo59crz@gmail.com>
2024-11-12 23:05:48 -0300

    disko

---
+++ disko.nix
@@ -0,0 +1,41 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/sda"; # disk
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            swap = {
+              size = "16G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
