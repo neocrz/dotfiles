9722c7791090e122a6d2e73ff0a17ea7474a7d40
neocrz <neo59crz@gmail.com>
2024-11-16 13:57:08 -0300

    yt-dlp nixvim

--- home/mod/bash.nix
+++ home/mod/bash.nix
@@ -3,7 +3,7 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      mp3 = "yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 ";
+      mp3 = ''yt-dlp -o "%(channel)s - %(title)s.%(ext)s" -x --audio-format mp3 '';
       mp4 = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 ";
       no-s = "sudo nixos-rebuild switch --flake ~/dotfiles";
       no-b = "sudo nixos-rebuild boot --flake ~/dotfiles";
