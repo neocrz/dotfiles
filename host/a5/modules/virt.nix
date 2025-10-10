0f9e7c9704332fb3dbb25d392ca2cb78a262d301
neocrz <neo59crz@gmail.com>
2025-10-10 12:54:36 -0300

    virt

--- host/a5/modules/virt.nix
+++ host/a5/modules/virt.nix
@@ -2,6 +2,11 @@
   virtualisation.docker = {
     enable = true;
     storageDriver = "btrfs";
+    daemon.settings = {
+      features = {
+        cdi = true;
+      };
+    };
   };
   hardware.nvidia-container-toolkit.enable = true;
   users.users.eee.extraGroups = ["docker"];
